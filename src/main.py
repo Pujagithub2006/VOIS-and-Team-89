d0f1d8ea22b79efc2ad66ca7e882db66c6d19080
Puja Nikam <pujasantoshnikam@gmail.com>
2025-12-25 19:24:42 +0530

    feat: displayed acceleration magnitude and fall possibility

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from sensors.mpu6050_simulator import get_motion_data
 from detection.motion_analysis import calculate_magnitude
+from detection.threshold_fall import detect_fall
 import time
 
-while True:
-    ax, ay, az = get_motion_data()
-    magnitude = calculate_magnitude(ax, ay, az)
+mode = "normal"
 
-    print(f"AX:{ax:.2f} AY:{ay:.2f} AZ:{az:.2f} | MAG:{magnitude:.2f}")
+while True:
+    ax, ay, az = get_motion_data(mode)
+    magnitude = calculate_magnitude(ax, ay, az)
+    fall = detect_fall(magnitude)
+
+    print(f"MAG:{magnitude:.2f} | FALL:{fall}")
+
     time.sleep(1)
