eaa5c27fef88a8a774ac8762e07d46c08cd05265
Puja Nikam <pujasantoshnikam@gmail.com>
2025-12-25 20:03:28 +0530

    feat: displayed acc mag, posture, fall spike, inactivity

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
 from sensors.mpu6050_simulator import get_motion_data
 from detection.motion_analysis import calculate_magnitude
 from detection.threshold_fall import detect_fall
+from detection.posture_detection import detect_posture
+from detection.inactivity import is_inactive
 import time
 
-mode = "normal"
+mode = "fall"  
+
+prev_magnitude = 0
 
 while True:
     ax, ay, az = get_motion_data(mode)
     magnitude = calculate_magnitude(ax, ay, az)
-    fall = detect_fall(magnitude)
 
-    print(f"MAG:{magnitude:.2f} | FALL:{fall}")
+    fall_spike = detect_fall(magnitude)
+    posture = detect_posture(az)
+    inactive = is_inactive(magnitude, prev_magnitude)
 
+    print(
+        f"MAG:{magnitude:.2f} | "
+        f"POSTURE:{posture} | "
+        f"SPIKE:{fall_spike} | "
+        f"INACTIVE:{inactive}"
+    )
+
+    prev_magnitude = magnitude
     time.sleep(1)
